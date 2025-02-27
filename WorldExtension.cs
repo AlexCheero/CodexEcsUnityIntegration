7d7331e1e1143d1f3fb33bf5e041b4d1368984c8
Alexander Cherkasov <alexcheero157@gmail.com>
2025-02-27 12:39:38 +0100

    ecs update

--- WorldExtension.cs
+++ WorldExtension.cs
@@ -6,8 +6,11 @@ namespace CodexFramework.CodexEcsUnityIntegration
     public static class WorldExtension
     {
 #if DEBUG
-        public static void UnityDebugEntity(this EcsWorld world, Entity entity, string msg = "") => world.UnityDebugEntity(entity.GetId(), msg);
-        public static void UnityDebugEntity(this EcsWorld world, int id, string msg = "") => Debug.Log(msg + world.DebugEntity(id));
+        public static void UnityDebugEntity(this EcsWorld world, Entity entity, bool printFields, string msg = "") =>
+            world.UnityDebugEntity(entity.GetId(), printFields, msg);
+
+        public static void UnityDebugEntity(this EcsWorld world, int id, bool printFields, string msg = "") =>
+            Debug.Log(msg + world.DebugEntity(id, printFields));
 #endif
     }
 }
