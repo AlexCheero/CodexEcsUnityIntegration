e1dd7d2bdc3d49f1cea35c360b5cb30809230777
Alexander Cherkasov <alexcheero157@gmail.com>
2025-04-06 18:21:14 +0200

    Component Views filter

---
+++ ComponentFilterAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodexFramework.CodexEcsUnityIntegration
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class ComponentFilterAttribute : PropertyAttribute
+    {
+        public Type[] RequiredComponents;
+        public Type[] ExcludedComponents;
+        public ComponentFilterAttribute(Type[] requiredComponents, Type[] excludedComponents = null)
+        {
+            RequiredComponents = requiredComponents;
+            ExcludedComponents = excludedComponents;
+        }
+    }
+}
---
+++ Editor/ComponentFilterDrawer.cs
@@ -0,0 +1,68 @@
+﻿using CodexFramework.CodexEcsUnityIntegration.Views;
+using UnityEditor;
+using UnityEngine;
+
+namespace CodexFramework.CodexEcsUnityIntegration.Editor
+{
+    [CustomPropertyDrawer(typeof(ComponentFilterAttribute))]
+    public class ComponentFilterDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            // Draw the object field as usual
+            var newValue =
+                EditorGUI.ObjectField(position, label, property.objectReferenceValue, typeof(EntityView), true);
+
+            // Validation
+            if (newValue != null)
+            {
+                var go = ((EntityView)newValue).gameObject;
+                var attr = (ComponentFilterAttribute)attribute;
+                foreach (var required in attr.RequiredComponents)
+                {
+                    if (go.GetComponent(required) != null)
+                        continue;
+                    Debug.LogError($"Missing component: {required.Name}");
+                    
+                    //CODEX_TODO: draws only for one frame, try to fix
+                    // EditorGUI.HelpBox(position, $"Missing component: {required.Name}", MessageType.Error);
+                    
+                    EditorUtility.DisplayDialog(
+                        $"Missing component: {required.Name}",
+                        $"Game object should have: {required.Name}",
+                        "ok"
+                    );
+                    
+                    newValue = null; // cancel the assignment
+                    goto ValidationFinished;
+                }
+                
+                foreach (var excluded in attr.ExcludedComponents)
+                {
+                    if (go.GetComponent(excluded) == null)
+                        continue;
+                    Debug.LogError($"Exclude component: {excluded.Name}");
+                    
+                    //CODEX_TODO: draws only for one frame, try to fix
+                    // EditorGUI.HelpBox(position, $"Exclude component: {excluded.Name}", MessageType.Error);
+                    
+                    EditorUtility.DisplayDialog(
+                        $"Exclude component: {excluded.Name}",
+                        $"Game object should have no: {excluded.Name}",
+                        "ok"
+                    );
+                    
+                    newValue = null; // cancel the assignment
+                    goto ValidationFinished;
+                }
+            }
+
+            ValidationFinished:
+            property.objectReferenceValue = newValue;
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
